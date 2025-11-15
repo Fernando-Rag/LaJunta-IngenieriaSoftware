ee3398c588a207c865170ec44ace2e01b06c4779
Ragnar7m7 <fernandoorellanadiaz2002@gmail.com>
2025-11-15 00:27:27 -0300

    feat(loginfuncional)

--- lajunta/android/build.gradle.kts
+++ lajunta/android/build.gradle.kts
@@ -24,9 +24,8 @@ tasks.register<Delete>("clean") {
 }
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    // Versión del plugin de Google Services definida aquí
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.google.gms.google-services") apply false
 }
